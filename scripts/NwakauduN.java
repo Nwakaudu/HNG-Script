7b1fc7d07d21084b61530b1d446c0c6f01cf3c0a
Nwakaudu <nwakaudunnaemeka@gmail.com>
2020-06-04 09:57:40 +0100

    Update NwakauduN.java

--- scripts/NwakauduN.java
+++ scripts/NwakauduN.java
@@ -2,9 +2,7 @@ package nwakaudu.nnaemeka;
 
 public class NwakauduN {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("My name is Nwakaudu Nnaemeka with HNGi7 ID [HNG-02313] and email nwakaudunnaemeka@gmail.com.");
-        System.out.println("I shall be using java for stage 2 task");
+        System.out.println("Hello, World, this is [Nwakaudu Nnaemeka] with HNGi7 ID [HNG-02313] and email [nwakaudunnaemeka@gmail.com] using [java] for stage 2 task");
 
     }
 }
