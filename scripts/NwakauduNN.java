daee17b6dfea4be1c17b8f23edb86cc710c77230
Nwakaudu <nwakaudunnaemeka@gmail.com>
2020-06-04 10:21:42 +0100

    Create NwakauduNN.java

---
+++ scripts/NwakauduNN.java
@@ -0,0 +1,8 @@
+package nwakaudu.nnaemeka;
+
+public class NwakauduN {
+    public static void main(String[] args) {
+        System.out.println("Hello, World, this is [Nwakaudu Nnaemeka] with HNGi7 ID [HNG-02313] and email [nwakaudunnaemeka@gmail.com] using [java] for stage 2 task");
+
+    }
+}
